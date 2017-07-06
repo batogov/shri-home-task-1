fb2be1495991e61e444e117b66d21563d6840d6c
Dmitriy Batogov <thesiamesecat19@gmail.com>
2017-07-06 15:09:33 +0300

    Добавил попап, исправил и доработал стили

---
+++ js/script.js
@@ -0,0 +1,29 @@
+var popup = document.querySelector('.popup');
+var popupBack = document.querySelector('.popup__back');
+var popupClose = document.querySelector('.popup__close');
+var imageWrapper = popup.querySelector('.popup__img-wrapper');
+var images = document.querySelectorAll('.gallery-item');
+
+function onImageClick(event) {
+    event.preventDefault();
+    
+    var image = document.createElement('img');
+    image.src = 'img/' + this.dataset.img;
+    
+    imageWrapper.appendChild(image);
+    popup.classList.remove('popup--hidden');
+}
+
+function closePopup(event) {
+    event.preventDefault();
+    
+    imageWrapper.innerHTML = '';
+    popup.classList.add('popup--hidden');
+}
+
+images.forEach(function(image) {
+    image.addEventListener('click', onImageClick);
+});
+
+popupBack.addEventListener('click', closePopup);
+popupClose.addEventListener('click', closePopup);
