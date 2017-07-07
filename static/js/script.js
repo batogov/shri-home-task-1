d9d34d9c980d5c6de51d11428772e310727d2612
Dmitriy Batogov <thesiamesecat19@gmail.com>
2017-07-07 16:19:45 +0300

    Добавил новые картинки (2x и 1x), спиннер + поправил стили

--- static/js/script.js
+++ static/js/script.js
@@ -3,22 +3,39 @@ var popupBack = document.querySelector('.popup__back');
 var popupClose = document.querySelector('.popup__close');
 var imageWrapper = popup.querySelector('.popup__img-wrapper');
 var images = document.querySelectorAll('.gallery-item');
+var spinner = document.querySelector('.spinner');
 
 function onImageClick(event) {
     event.preventDefault();
+
+    spinner.classList.remove('spinner--hidden');
     
     var image = document.createElement('img');
-    image.src = 'static/img/' + this.dataset.img;
+    image.classList.add('popup__img', 'popup__img--hidden');
+    image.src = 'static/img/1x/' + this.dataset.img;
+    image.srcset = 'static/img/2x/' + this.dataset.img + ' 2x';
+
+    image.addEventListener('load', function() {
+        image.classList.remove('popup__img--hidden');
+        spinner.classList.add('spinner--hidden');
+    });
     
-    imageWrapper.appendChild(image);
     popup.classList.remove('popup--hidden');
+
+    console.log(imageWrapper);
+    imageWrapper.insertBefore(image, spinner);
+
+    document.body.classList.add('body-no-scroll');
 }
 
 function closePopup(event) {
-    event.preventDefault();
+    if (event) {
+        event.preventDefault();
+    }
     
-    imageWrapper.innerHTML = '';
+    imageWrapper.removeChild(imageWrapper.querySelector('.popup__img'));
     popup.classList.add('popup--hidden');
+    document.body.classList.remove('body-no-scroll');
 }
 
 images.forEach(function(image) {
@@ -31,3 +48,11 @@ popupClose.addEventListener('click', closePopup);
 document.addEventListener("DOMContentLoaded", function() {
     objectFitImages();
 });
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+        if (!popup.classList.contains('hidden')) {
+            closePopup();
+        }
+    }
+})
